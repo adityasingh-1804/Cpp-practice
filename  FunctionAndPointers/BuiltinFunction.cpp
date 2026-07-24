29730d31f37ea19f551f32ec944a8d684912374b
Your Actual GitHub Username <NEW_EMAIL@example.com>
2026-07-24 23:27:12 +0530

    first commit

---
+++  FunctionAndPointers/BuiltinFunction.cpp
@@ -0,0 +1,5 @@
+//squr()
+//cqur()
+//min()
+//max()
+//power(a,b)
---
+++  FunctionAndPointers/Permutation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+// int add(int x){
+//     int f=1;
+//     for(int i=1;i<=x;i++){
+//         f = f*i;
+//     }
+//         return f;
+    
+// }
+    int main(){
+        int n;
+        cout<< "enter the value of n:";
+        cin>> n;
+        int r;
+        cout<< "enter the value of r:";
+        cin>> r;
+        int a=n;
+        int b=(n-r);
+        
+        cout<< a/b;
+       
+    }
+
+
