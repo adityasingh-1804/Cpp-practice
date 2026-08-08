7b798d1cf9ed5271834f4bee62fc4abbe0d89eaa
adityasingh-1804 <adityasingh12092007@gmail.com>
2026-08-09 00:21:54 +0530

    strigs last part

---
+++ computethefacrifprime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n,i;
+  cout<< " Enter the value of N : ";
+  cin>> n;
+  int fac=1;
+  for(i=1;i<=n;i++){
+if(n%2!=0){
+    
+}
+    fac=fac*i;
+
+
+  }
+cout<< fac;
+    
+  return 0;
+}
